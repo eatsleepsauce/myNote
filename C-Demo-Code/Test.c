c89c958801b8ff2f3e818351c365f784c8d7254f
yangliu07 <yangliu07@wisedu.com>
2020-12-21 20:06:26 +0800

    笔记

--- C-Demo-Code/Test.c
+++ C-Demo-Code/Test.c
@@ -5,5 +5,11 @@ int main(int argc, char const *argv[])
     /* code */
     printf("测试首都基辅罗斯大家发\n");
     printf("测试首都基辅罗斯大家发\n");
+    int test = 100;
+    if (test<99)
+    {
+       printf("11111");
+    }
+    
     return 0;
 }
